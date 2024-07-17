f8889c3aa936b7aeb9379b8f5cff4dde8df2e87e
Sandesh Sarade <Saradesandesh@gmail.com>
2024-07-17 16:43:53 +0530

    running map function with frame work

--- src/Sandesh.java
+++ src/Sandesh.java
@@ -1,5 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 class Sandesh {
+
   public static void main(String[] args) {
-    System.out.println("Hello");
+    Map<Integer,String> map = new HashMap<>();
+    map.put(1,"One");
+    map.put(2,"TWo");
+    map.put(3,"Three");
+
+   for (Integer key : map.keySet()) {
+    System.out.println("Key is "+key +" value is "+ map.get(key));
+   }
   }
 }
