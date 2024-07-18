5bda598e1c8052f9d0b1392eb3b491ec4151a290
Sandesh Sarade <Saradesandesh@gmail.com>
2024-07-18 20:15:32 +0530

    remain comaprator

---
+++ src/EmployeeTest.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class EmployeeTest {
+  public static void main(String[] args) {
+    
+    List<Employee> emp = new ArrayList<>();
+    emp.add(new Employee(101, "Sandesh", "Pune"));
+    emp.add(new Employee(102, "Piyush", "Mohadi"));
+    emp.add(new Employee(103, "Rakesh", "Bhandara"));
+    emp.add(new Employee(104, "Amol", "Pune"));
+
+    System.out.println(emp);
+    Collections.sort(null);
+  }
+}
